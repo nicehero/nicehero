47f7272b44ef83762c7503403db9db0ef608b3ea
nicehero <85948597@qq.com>
2019-03-20 02:34:12 +0800

    good job mongoc

--- dep/build.py
+++ dep/build.py
@@ -1,49 +1,72 @@
 import os
+def do_os(cmd):
+	b = os.system(cmd)
+	if b != 0:
+		exit(1)
+do_os('rm -rf include')
+do_os('rm -rf lib')
 
-os.system('rm -rf include')
-os.system('rm -rf lib')
-
-os.system('mkdir include')
-os.system('mkdir lib')
+do_os('mkdir include')
+do_os('mkdir lib')
 print 'download micro-ecc'
 
-os.system("wget https://github.com/nicehero/micro-ecc/archive/v1.0.tar.gz")
-os.system("tar xvf v1.0.tar.gz")
-os.system("mv micro-ecc-1.0 include/micro-ecc")
-os.system("rm -rf v1.0.tar.gz")
+do_os("wget https://github.com/nicehero/micro-ecc/archive/v1.0.tar.gz")
+do_os("tar xvf v1.0.tar.gz")
+do_os("mv micro-ecc-1.0 include/micro-ecc")
+do_os("rm -rf v1.0.tar.gz")
 
 print 'build micro-ecc'
 
 if os.name == "nt":
-	os.system("gcc -c include/micro-ecc/uECC.c")
-	os.system("ar -r lib/uECC.lib uECC.o")
+	do_os("gcc -c include/micro-ecc/uECC.c")
+	do_os("ar -r lib/uECC.lib uECC.o")
 else:
-	#os.system("gcc -c include/micro-ecc/uECC.c")
-	#os.system("ar -rcs lib/libuECC.a uECC.o")
-	os.system("gcc -shared -fPIC -o lib/libuECC.so include/micro-ecc/uECC.c")
-os.system("rm -rf uECC.o")
+	#do_os("gcc -c include/micro-ecc/uECC.c")
+	#do_os("ar -rcs lib/libuECC.a uECC.o")
+	do_os("gcc -shared -fPIC -o lib/libuECC.so include/micro-ecc/uECC.c")
+do_os("rm -rf uECC.o")
 
 print 'end micro-ecc'
 
 print 'download asio'
-os.system("wget https://github.com/chriskohlhoff/asio/archive/asio-1-12-2.tar.gz")
-os.system("tar xvf asio-1-12-2.tar.gz")
-os.system("mv asio-asio-1-12-2/asio/include include/asio")
-os.system("rm -rf asio-asio-1-12-2")
-os.system("rm -rf asio-1-12-2.tar.gz")
+do_os("wget https://github.com/chriskohlhoff/asio/archive/asio-1-12-2.tar.gz")
+do_os("tar xvf asio-1-12-2.tar.gz")
+do_os("mv asio-asio-1-12-2/asio/include include/asio")
+do_os("rm -rf asio-asio-1-12-2")
+do_os("rm -rf asio-1-12-2.tar.gz")
 
 print 'download tiny_sha3'
-os.system("git clone https://github.com/mjosaarinen/tiny_sha3")
-os.system("mv tiny_sha3 include/")
+do_os("git clone https://github.com/mjosaarinen/tiny_sha3")
+do_os("mv tiny_sha3 include/")
 if os.name == "nt":
-	os.system("gcc -c include/tiny_sha3/sha3.c")
-	os.system("ar -r lib/sha3.lib sha3.o")
+	do_os("gcc -c include/tiny_sha3/sha3.c")
+	do_os("ar -r lib/sha3.lib sha3.o")
 else:
-	#os.system("gcc -c include/tiny_sha3/sha3.c")
-	#os.system("ar -rcs lib/libsha3.a sha3.o")
-	os.system("gcc -shared -fPIC -o lib/libsha3.so include/tiny_sha3/sha3.c")
-os.system("rm -rf sha3.o")
+	#do_os("gcc -c include/tiny_sha3/sha3.c")
+	#do_os("ar -rcs lib/libsha3.a sha3.o")
+	do_os("gcc -shared -fPIC -o lib/libsha3.so include/tiny_sha3/sha3.c")
+do_os("rm -rf sha3.o")
+
+print 'download mongo-c' #mongo-c-driver-1.14.0.tar.gz
+print 'build mongoc'
+do_os('tar xvf mongo-c-driver-1.14.0.tar.gz')
+do_os('mkdir build_mongoc')
+os.chdir('./build_mongoc')
+if os.name == "nt":
+	p = os.getcwd()
+	do_os('cmake -G "Visual Studio 14 2015 Win64" "-DCMAKE_INSTALL_PREFIX=%s\\..\\mongo-c-driver" "-DCMAKE_PREFIX_PATH=%s\\..\\mongo-c-driver" ..\\mongo-c-driver-1.14.0'%(p,p))
+	do_os('devenv mongo-c-driver.sln /Build "RelWithDebInfo|x64"')
+	do_os('devenv mongo-c-driver.sln /Build "RelWithDebInfo|x64" /project INSTALL')
+	os.chdir('../')
+	do_os('mv mongo-c-driver/include/libmongoc-1.0/mongoc include/')
+	do_os('mv mongo-c-driver/include/libbson-1.0/bson include/')
+	do_os('mv mongo-c-driver/lib/* lib/')
+	do_os('mv mongo-c-driver/bin/* lib/')
+	do_os('rm -rf mongo-c-driver')
+	do_os('rm -rf build_mongoc')
+do_os('rm -rf mongo-c-driver-1.14.0')
+do_os('rm -rf mongo-c-driver-1.14.0.tar.gz')
 
 
-os.system("echo 0.1 > done")
+do_os("echo 0.1 > done")
 print 'done'
