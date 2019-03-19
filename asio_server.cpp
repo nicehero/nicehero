9b42462241a31eef0a769fef0358d49b7d8895a5
nicehero <85948597@qq.com>
2019-03-19 17:37:46 +0800

    good job

--- asio_server.cpp
+++ asio_server.cpp
@@ -184,7 +184,7 @@ SESSION_COMMAND(MyClient, 100)
 static int recv101Num = 0;
 SESSION_COMMAND(MyClient, 101)
 {
-	nlog("recv101");
+	nlog("recv101 recv101Num:%d", recv101Num);
 	++recv101Num;
 	return true;
 }
