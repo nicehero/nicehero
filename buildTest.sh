57679dcc1aea74b1c879d50c29591c0fa5055234
nicehero <85948597@qq.com>
2019-03-29 18:34:29 +0800

    good job

--- buildTest.sh
+++ buildTest.sh
@@ -1,3 +1,3 @@
-g++ -g -Wall -std=c++11  -I./dep/include -I./dep/include/asio -L./dep/lib -lpthread -lsha3 -luECC -DASIO_STANDALONE Log.cpp Clock.cpp Server.cpp client_test.cpp Tcp.cpp Service.cpp TestProtocolCmd.cpp -o testClient -Wl,-rpath=dep/lib
+g++ -g -Wall -std=c++11  -I./dep/include -I./dep/include/asio -L./dep/lib -lpthread -lsha3 -luECC -lmongoc-1.0 -lbson-1.0 -DASIO_STANDALONE Log.cpp Clock.cpp Server.cpp client_test.cpp Tcp.cpp Service.cpp TestProtocolCmd.cpp -o testClient -Wl,-rpath=dep/lib
 g++ -g -Wall -std=c++11  -I./dep/include -I./dep/include/asio -L./dep/lib -lpthread -lsha3 -luECC -DASIO_STANDALONE Log.cpp Clock.cpp Server.cpp asio_server.cpp Tcp.cpp Service.cpp TestProtocolCmd.cpp -o testServer -Wl,-rpath=dep/lib
 
